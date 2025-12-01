e437d6c8dae65c195b3e96903a284192a917b195
Danijel Matic <danijel.matic@ericsson.com>
2025-12-01 14:58:21 +0100

    Ispravke u CheckingAmstrongNum.java

--- src/CheckingAmstrongNum.java
+++ src/CheckingAmstrongNum.java
@@ -1,5 +1,3 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class CheckingAmstrongNum{
 
     public static boolean checkingArmstrong(int broj) {
